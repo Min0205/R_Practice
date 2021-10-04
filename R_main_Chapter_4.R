2bdc5ae84c5789047f57d6f07d364b677d44c8b6
min0205 <zhaomin0205@gmail.com>
2021-10-04 11:38:02 +0800

    customizing and choosing colors

--- R_main_Chapter_4.R
+++ R_main_Chapter_4.R
@@ -91,3 +91,18 @@ plot(height ~
 legend("bottomright", levels(allom$species), pch=15, col=palette(), title="Species")
 
 ##fine-tuning the format of plots
+##customizing and choosing colors
+plot(x,y, col=3)
+colors()
+palette(c("blue2","goldenrod1","firebrick2","chartreuse4",
+          "deepskyblue1","darkorange1","darkorchid3","darkgrey",
+          "mediumpurple1","orangered2","chocolate","burlywood3",
+          "goldenrod4","darkolivegreen2","palevioletred3",
+          "darkseagreen3","sandybrown","tan",
+          "gold","violetred4","darkgreen"))
+# A simple graph showing the colours.
+par(cex.axis=0.8, las=3)
+n <- length(palette())
+barplot(rep(1,n),
+        col=1:n,
+        names.arg=1:n,axes=FALSE)
